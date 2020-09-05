f6c4d7e8e4153c1863f912580927f5f0abe611a7
sAksham-Ar <aryasaksham@gmail.com>
2020-09-06 01:02:44 +0530

    gallery

--- images/img.sh
+++ images/img.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+a=0
+echo "<div class=\"row\">"
 for i in $(ls)
- do 
- echo "<div class=\"swiper-slide\"><div class=\"swiper-zoom-container\"><img src=\"images/$i\" /></div></div>"
+ do
+ let a++
+ echo "<div class=\"col-sm-3\"><img src=\"images/$i\"></div>"
+ if(($a%4== 0))
+ then
+  echo "</div><div class=\"row\">"
+ fi
  done
+echo "</div>"
