a7b71fca59348c3317c5293c1c555481c04821ef
sAksham-Ar <aryasaksham@gmail.com>
2020-09-08 16:03:20 +0530

    gallery ver 2

--- images/img.sh
+++ images/img.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 a=0
 echo "<div class=\"row\">"
-for i in $(ls)
+for i in $(ls *.JPG)
  do
  let a++
  echo "<div class=\"col-sm-3\"><img src=\"images/$i\"></div>"
  if(($a%4== 0))
  then
-  echo "</div><div class=\"row\">"
+   echo "</div><div class=\"row\">"
  fi
  done
 echo "</div>"
